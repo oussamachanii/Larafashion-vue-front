0e0587dd21c99a251712b56e0c08b28b246b8902
oussama chanii <chaniioussama1@gmail.com>
2021-04-08 23:46:00 +0100

    completting purchases page

--- src/pages/admin/Purchases.vue
+++ src/pages/admin/Purchases.vue
@@ -1,12 +1,25 @@
 <template>
+  <DeleteConfirmation
+    @cancel="cancel"
+    @delete="Delete"
+    v-if="inDelete"
+    message="are you sur that you want to delete this purchase"
+    :user="inDeletePurchase"
+  ></DeleteConfirmation>
   <header class="flex ">
     <h1 class="text-3xl font-bold text-gray-700 mb-4">
       Purchases
     </h1>
   </header>
   <div class="flex  justify-between  my-6 ">
-    <input type="text" class="flex-grow focus:bg-gray-400 py-3" />
+    <input
+      type="text"
+      class="flex-grow focus:bg-gray-400 py-3"
+      v-model="search"
+      @keypress.enter="searchPurchase"
+    />
     <button
+      @click="searchPurchase"
       class="  w-20 items-center px-4 py-2  ml-4 border border-transparent rounded-md shadow-sm   text-white bg-current hover:bg-current-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-current-light"
     >
       <svg
@@ -23,8 +36,15 @@
       </svg>
     </button>
   </div>
-  <div class="mt-4">
-    <Table>
+  <div class="mt-4 w-full ">
+    <p v-if="loading" class="text-xl font-semibold text-current">Loading...</p>
+    <p
+      v-if="purchases.data?.length == 0"
+      class="text-xl font-semibold text-current text-center"
+    >
+      no purchases available!
+    </p>
+    <Table v-else>
       <template #Header>
         <th
           class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider"
@@ -35,7 +55,7 @@
         </th>
       </template>
       <template #Content>
-        <tr v-for="purchase in purchases" :key="purchase.product_id">
+        <tr v-for="purchase in purchases.data" :key="purchase.id">
           <td class="px-6 py-4 whitespace-nowrap">
             <div class="text-md font-semibold text-gray-900">
               {{ purchase.purchaser }}
@@ -55,10 +75,12 @@
               {{ purchase.size }}
             </div>
           </td>
-          <td class="px-6 py-4 whitespace-nowrap">
-            <div class="text-md font-semibold text-gray-900">
-              {{ purchase.color }}
-            </div>
+          <td class="px-6  py-4 whitespace-nowrap">
+            <!-- {{ purchase.color }} -->
+            <p
+              :style="'background-color:' + purchase.color + ';'"
+              class=" border px-2 py-4 mr-2 rounded-full"
+            ></p>
           </td>
           <td class="px-6 py-4 whitespace-nowrap">
             <div class="text-md font-semibold text-gray-900">
@@ -82,6 +104,7 @@
           >
             <button
               class="my-auto px-3 py-1 border border-transparent rounded-lg shadow-md  bg-red-600 hover:bg-red-700 focus:outline-none "
+              @click="Delete(purchase.id)"
             >
               <svg
                 class="w-6 h-6 stroke-current text-white"
@@ -101,16 +124,30 @@
           </td>
         </tr>
       </template>
+      <template #Pagination>
+        <Pagination
+          @paginate="paginate"
+          :links="purchases?.links"
+          :meta="purchases?.meta"
+        ></Pagination>
+      </template>
     </Table>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex";
+import axios from "axios";
 import Table from "../../components/Table.vue";
+import Pagination from "../../components/Pagination.vue";
+import DeleteConfirmation from "../../components/DeleteConfirmation.vue";
 export default {
-  components: { Table },
+  components: { Table, DeleteConfirmation, Pagination },
   setup() {
+    const store = useStore();
+    const loading = ref(true);
+    const search = ref(null);
     const ths = ref([
       "Purchaser",
       "Product_id",
@@ -120,70 +157,101 @@ export default {
       "date",
       "Total Price",
     ]);
-    const purchases = ref([
-      {
-        purchaser: "oussama chanii",
-        product_id: "1232312",
-        email: "chaniioussama1@gmail.com",
-        size: "XL , XLL , XLLL",
-        color: "Gray",
-        quantity: "12",
-        date: "12/12/2020 12:22",
-        price: "33",
-      },
-      {
-        purchaser: "oussama chanii",
-        product_id: "123231223",
-        email: "chaniioussama1@gmail.com",
-        size: "XL , XLL , XLLL",
-        color: "Gray",
-        quantity: "12",
-        date: "12/12/2020 12:22",
-        price: "33",
-      },
-      {
-        purchaser: "oussama chanii",
-        product_id: "12323232312",
-        email: "chaniioussama1@gmail.com",
-        size: "XL , XLL , XLLL",
-        color: "Gray",
-        quantity: "12",
-        date: "12/12/2020 12:22",
-        price: "33",
-      },
-      {
-        purchaser: "oussama chanii",
-        product_id: "123223312",
-        email: "chaniioussama1@gmail.com",
-        size: "XL , XLL , XLLL",
-        color: "Gray",
-        quantity: "12",
-        date: "12/12/2020 12:22",
-        price: "33",
-      },
-      {
-        purchaser: "oussama chanii",
-        product_id: "1232311112",
-        email: "chaniioussama1@gmail.com",
-        size: "XL , XLL , XLLL",
-        color: "Gray",
-        quantity: "12",
-        date: "12/12/2020 12:22",
-        price: "33",
-      },
-      {
-        purchaser: "oussama chanii",
-        product_id: "123232314321412",
-        email: "chaniioussama1@gmail.com",
-        size: "XL , XLL , XLLL",
-        color: "Gray",
-        quantity: "12",
-        date: "12/12/2020 12:22",
-        price: "33",
-      },
-    ]);
+    const purchases = ref({ data: [], meta: {}, links: {} });
+    const config = {
+      headers: { Authorization: `Bearer ${store.state.auth.api_token}` },
+      params: { pagination: localStorage.getItem("pagination") },
+    };
+    const paginate = (url) => {
+      getData(url);
+    };
+    const searchPurchase = () => {
+      if (!search.value) {
+        return getData();
+      }
+      return axios
+        .get("purchase/", {
+          headers: { Authorization: `Bearer ${store.state.auth.api_token}` },
+          params: {
+            pagination: localStorage.getItem("pagination"),
+            search: search.value,
+          },
+        })
+        .then((response) => {
+          purchases.value = response.data;
+        })
+        .catch((error) => {
+          store.commit("setToast", {
+            message: error.message,
+            type: "error",
+          });
+        })
+        .finally((loading.value = false));
+    };
+    const getData = (url = "purchase") => {
+      loading.value = true;
+      axios
+        .get(url, config)
+        .then((response) => {
+          purchases.value = response.data;
+        })
+        .catch((error) => {
+          console.log(error.message.status);
+          store.commit("setToast", {
+            message: error.message,
+            type: "error",
+          });
+        })
+        .finally((loading.value = false));
+    };
+    onMounted(() => {
+      getData();
+    });
+    const inDelete = ref(false);
+    const inDeletePurchase = ref(null);
+    const Delete = (id = null) => {
+      if (!inDeletePurchase.value || !id) {
+        inDelete.value = true;
+        inDeletePurchase.value = id;
+      } else {
+        purchases.value.data = purchases.value.data.filter(
+          (purchase) => purchase.id != id
+        );
+        axios
+          .delete("purchase/" + id, config)
+          .then(() => {
+            store.commit("setToast", {
+              message: "purchase Successfully Deleted",
+              type: "success",
+            });
+            cancel();
+          })
+          .catch((error) => {
+            store.commit("setToast", {
+              message: error.message,
+              type: "error",
+            });
+            cancel();
+          });
+      }
+    };
+    const cancel = () => {
+      inDelete.value = false;
+      inDeletePurchase.value = null;
+    };
 
-    return { ths, purchases };
+    return {
+      ths,
+      purchases,
+      loading,
+      paginate,
+      search,
+      searchPurchase,
+      inDelete,
+      cancel,
+      Delete,
+      inDeletePurchase,
+    };
   },
 };
 </script>
