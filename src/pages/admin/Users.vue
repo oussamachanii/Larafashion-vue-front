9a60609d2c0f4b4d3015bde398c9f3f6a576225c
oussama chanii <chaniioussama1@gmail.com>
2021-04-08 15:36:50 +0100

    completting Users page

--- src/pages/admin/Users.vue
+++ src/pages/admin/Users.vue
@@ -1,12 +1,25 @@
 <template>
+  <DeleteConfirmation
+    @cancel="cancel"
+    @delete="Delete"
+    v-if="inDelete"
+    message="are you sur that you want to delete this user"
+    :user="inDeleteUser"
+  ></DeleteConfirmation>
   <header class="flex ">
     <h1 class="text-3xl font-bold text-gray-700 mb-4">
       Users
     </h1>
   </header>
   <div class="flex  justify-between  my-6 ">
-    <input type="text" class="flex-grow focus:bg-gray-400 py-3" />
+    <input
+      type="text"
+      class="flex-grow focus:bg-gray-400 py-3"
+      v-model="search"
+      @keypress.enter="searchUser"
+    />
     <button
+      @click="searchUser"
       class="  w-20 items-center px-4 py-2  ml-4 border border-transparent rounded-md shadow-sm   text-white bg-current hover:bg-current-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-current-light"
     >
       <svg
@@ -23,11 +36,18 @@
       </svg>
     </button>
   </div>
-  <div class="mt-4">
-    <Table>
+  <div class="mt-4 w-full ">
+    <p v-if="loading" class="text-xl font-semibold text-current">Loading...</p>
+    <p
+      v-if="users.data.length == 0"
+      class="text-xl font-semibold text-current text-center"
+    >
+      no users available!
+    </p>
+    <Table v-else>
       <template #Header>
         <th
-          class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider"
+          class="px-2 py-3 text-left text-xs  text-gray-600 uppercase tracking-wider"
           v-for="th in ths"
           :key="th"
         >
@@ -35,37 +55,42 @@
         </th>
       </template>
       <template #Content>
-        <tr v-for="user in users" :key="user.name">
-          <td class="px-6 py-4 whitespace-nowrap">
+        <tr v-for="user in users.data" :key="user.id">
+          <td class="px-2 py-4 whitespace-nowrap">
             <div class="text-md font-semibold text-gray-900">
               {{ user.name }}
             </div>
           </td>
-          <td class="px-6 py-4 whitespace-nowrap">
+          <td class="px-2 py-4 whitespace-nowrap">
             <div class="text-md font-semibold text-gray-900">
               {{ user.email }}
             </div>
           </td>
-          <td class="px-6 py-4 whitespace-nowrap">
-            <div class="text-md font-semibold text-gray-900">
+
+          <td class="px-2 py-4 whitespace-nowrap">
+            <div class="text-md w-44 font-semibold text-gray-900 truncate">
+              {{ user.location }}
+            </div>
+          </td>
+          <td class="px-2 py-4 whitespace-nowrap">
+            <div class="text-md w-44 font-semibold text-gray-900 truncate">
               {{ user.address }}
             </div>
           </td>
-          <td class="px-6 py-4 whitespace-nowrap">
+          <td class="px-2 py-4 whitespace-nowrap">
             <div class="text-md font-semibold text-gray-900">
               {{ user.role == 0 ? "user" : "admin" }}
             </div>
           </td>
-          <td class="px-6 py-4 whitespace-nowrap">
+          <td class="px-2 py-4 whitespace-nowrap">
             <div class="text-md font-semibold text-gray-900">
               {{ user.registerDate }}
             </div>
           </td>
-          <td
-            class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
-          >
+          <td class="px-2 py-4 whitespace-nowrap text-right text-sm ">
             <button
               class="my-auto px-3 py-1 border border-transparent rounded-lg shadow-md  bg-red-600 hover:bg-red-700 focus:outline-none "
+              @click="Delete(user.id)"
             >
               <svg
                 class="w-6 h-6 stroke-current text-white"
@@ -85,42 +110,133 @@
           </td>
         </tr>
       </template>
+      <template #Pagination>
+        <Pagination
+          @paginate="paginate"
+          :links="users?.links"
+          :meta="users?.meta"
+        ></Pagination>
+      </template>
     </Table>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex";
+import axios from "axios";
 import Table from "../../components/Table.vue";
+import Pagination from "../../components/Pagination.vue";
+import DeleteConfirmation from "../../components/DeleteConfirmation.vue";
 export default {
-  components: { Table },
+  components: { Table, Pagination, DeleteConfirmation },
   setup() {
-    const ths = ref(["Name", "Email", "Address", "Role", "Register Date"]);
-    const users = ref([
-      {
-        name: "oussama chanii",
-        email: "chaniioussama1@gmail.com",
-        address: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        role: 1,
-        registerDate: "12/12/2020 12:22",
-      },
-      {
-        name: "oussama ",
-        email: "chaniioussama1@gmail.com",
-        address: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        role: 1,
-        registerDate: "12/12/2020 12:22",
-      },
-      {
-        name: "oussama chaniii",
-        email: "chaniioussama1@gmail.com",
-        address: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-        role: 1,
-        registerDate: "12/12/2020 12:22",
-      },
+    const store = useStore();
+    const loading = ref(true);
+    const search = ref(null);
+    const ths = ref([
+      "Name",
+      "Email",
+      "Location",
+      "Address",
+      "Role",
+      "Register Date",
     ]);
 
-    return { ths, users };
+    const users = ref({ data: [], meta: {}, links: {} });
+    const config = {
+      headers: { Authorization: `Bearer ${store.state.auth.api_token}` },
+      params: { pagination: localStorage.getItem("pagination") },
+    };
+    const paginate = (url) => {
+      getData(url);
+    };
+    const searchUser = () => {
+      if (!search.value) {
+        return getData();
+      }
+      return axios
+        .get("user/", {
+          headers: { Authorization: `Bearer ${store.state.auth.api_token}` },
+          params: {
+            pagination: localStorage.getItem("pagination"),
+            search: search.value,
+          },
+        })
+        .then((response) => {
+          users.value = response.data;
+        })
+        .catch((error) => {
+          console.log(error.message.status);
+          store.commit("setToast", {
+            message: error.message,
+            type: "error",
+          });
+        })
+        .finally((loading.value = false));
+    };
+    const getData = (url = "user") => {
+      loading.value = true;
+      axios
+        .get(url, config)
+        .then((response) => {
+          users.value = response.data;
+        })
+        .catch((error) => {
+          console.log(error.message.status);
+          store.commit("setToast", {
+            message: error.message,
+            type: "error",
+          });
+        })
+        .finally((loading.value = false));
+    };
+
+    onMounted(() => {
+      getData();
+    });
+    const inDelete = ref(false);
+    const inDeleteUser = ref(null);
+    const Delete = (id = null) => {
+      if (!inDeleteUser.value || !id) {
+        inDelete.value = true;
+        inDeleteUser.value = id;
+      } else {
+        axios
+          .delete("user/" + id, config)
+          .then(() => {
+            users.value.data = users.value.data.filter((user) => user.id != id);
+            store.commit("setToast", {
+              message: "User Successfuly Deleted",
+              type: "success",
+            });
+            cancel();
+          })
+          .catch((error) => {
+            store.commit("setToast", {
+              message: error.message,
+              type: "error",
+            });
+            cancel();
+          });
+      }
+    };
+    const cancel = () => {
+      inDelete.value = false;
+      inDeleteUser.value = null;
+    };
+    return {
+      ths,
+      users,
+      loading,
+      paginate,
+      search,
+      searchUser,
+      inDelete,
+      cancel,
+      Delete,
+      inDeleteUser,
+    };
   },
 };
 </script>
