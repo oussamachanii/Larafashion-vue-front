abfe131e6e583de91b36a10dca1e5b35f0f8e214
oussama chanii <chaniioussama1@gmail.com>
2021-04-08 15:35:38 +0100

    adding pagination to table via slot

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,159 +1,24 @@
 <template>
-  <div class="flex flex-col">
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+  <div class="flex flex-col w-full shadow-md">
+    <div class="-my-2 overflow-x-auto ">
+      <div class="py-2 align-middle inline-block w-full overflow-y-auto ">
         <div
           class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
         >
-          <table class="min-w-full divide-y divide-gray-200">
+          <table class="divide-y divide-gray-200 w-full">
             <thead class="bg-gray-50">
               <tr>
                 <slot name="Header"></slot>
-                <th scope="col" class="relative px-6 py-3">
+                <th scope="col" class="relative  py-3">
                   <!-- <span class="sr-only">Edit</span> -->
                 </th>
               </tr>
             </thead>
-            <tbody class="bg-white divide-y divide-gray-200">
+            <tbody class="bg-white  divide-gray-200">
               <slot name="Content"></slot>
-              <!-- <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-md font-semibold text-gray-900">
-                    Regional Paradigm Technician
-                  </div>
-                </td> 
-                <slot name='Content'></slot>
-                <td
-                  class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
-                >
-                  <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                    >Edit</a
-                  >
-                </td>
-              </tr> -->
-
-              <!-- More items... -->
             </tbody>
           </table>
-          <div class="bg-green-900  ">
-            <div
-              class="bg-white px-4 py-3 w-full flex items-center justify-between border-t border-gray-200 sm:px-6"
-            >
-              <div class="flex-1 flex justify-between sm:hidden">
-                <a
-                  href="#"
-                  class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-                >
-                  Previous
-                </a>
-                <a
-                  href="#"
-                  class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-                >
-                  Next
-                </a>
-              </div>
-              <div
-                class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between"
-              >
-                <div>
-                  <p class="text-sm text-gray-700">
-                    Total
-                    <span class="font-semibold">total</span>
-                  </p>
-                </div>
-                <div>
-                  <nav
-                    class="relative z-0 inline-flex shadow-sm -space-x-px"
-                    aria-label="Pagination"
-                  >
-                    <a
-                      href="#"
-                      class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                    >
-                      <span class="sr-only">Previous</span>
-                      <!-- Heroicon name: solid/chevron-left -->
-                      <svg
-                        class="h-5 w-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                    </a>
-
-                    <a
-                      href="#"
-                      class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-                    >
-                      1
-                    </a>
-                    <a
-                      href="#"
-                      class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-                    >
-                      2
-                    </a>
-                    <a
-                      href="#"
-                      class="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-                    >
-                      3
-                    </a>
-                    <span
-                      class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700"
-                    >
-                      ...
-                    </span>
-                    <a
-                      href="#"
-                      class="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-                    >
-                      8
-                    </a>
-                    <a
-                      href="#"
-                      class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-                    >
-                      9
-                    </a>
-                    <a
-                      href="#"
-                      class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-                    >
-                      10
-                    </a>
-                    <a
-                      href="#"
-                      class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                    >
-                      <span class="sr-only">Next</span>
-                      <!-- Heroicon name: solid/chevron-right -->
-                      <svg
-                        class="h-5 w-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                    </a>
-                  </nav>
-                </div>
-              </div>
-            </div>
-          </div>
+          <slot name="Pagination"></slot>
         </div>
       </div>
     </div>
